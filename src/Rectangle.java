d546f2ae5d1c43723db653a7d1464161b4e65710
smithb <smithb@wlwv.k12.or.us>
2023-01-30 22:04:20 -0800

    intial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,27 @@
+import java.text.DecimalFormat;
+public class Rectangle { //make public for CS A
+
+    //instance variables
+
+    //default constructor (make public for CS A)
+
+
+    //overloaded constructor (with parameters)
+
+
+
+    //accessors
+
+
+
+    //mutators
+
+
+    //other methods
+
+
+
+
+    //toString method
+
+}
