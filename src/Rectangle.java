3255fd33f67029a8e150acb4d0ff72b3c6106081
shahawlhs <shaha@wlhs.wlwv.k12.or.us>
2023-01-31 09:23:21 -0800

    rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,11 +2,27 @@ import java.text.DecimalFormat;
 public class Rectangle { //make public for CS A
 
     //instance variables
+    private double width;
+    private double height;
+    private double area;
+    private double perimeter;
 
     //default constructor (make public for CS A)
+    public Rectangle(){
+        width = 1.0;
+        height = 1.0;
+    }
 
 
     //overloaded constructor (with parameters)
+    public Rectangle(double w, double h){
+        width = w;
+        height = h;
+    }
+
+    public Rectangle(double width){
+        this.width = width;
+    }
 
 
 
